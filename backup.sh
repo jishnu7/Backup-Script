41baa5869a844b7f87b0ab6f9fd0df5237872538
Jishnu <jishnu7@gmail.com>
2011-11-04 12:31:26 +0530

    error handling

--- backup.sh
+++ backup.sh
@@ -19,14 +19,13 @@ function list_backup {
 
 case $1 in
     init)
-        if [ -d $BACKUP_FOLDER ]; then
             touch $BACKUP_FOLDER/list
-            echo "Backup already initialized."
-        else 
-            mkdir $BACKUP_FOLDER
-            touch $BACKUP_FOLDER/list
-            echo "Backup folder initialized"
-        fi
+            mkdir -p $BACKUP_FOLDER
+            if [[ $? -ne 0 ]] ; then
+                exit 1
+            else
+                echo "Backup folder initialized"
+            fi
         ;;
     backup)
         if [ $2 ]; then
@@ -57,8 +56,13 @@ case $1 in
 
                 if [ "$flag" == "TRUE" ]; then
                     cp -u -r -i $var $BACKUP_FOLDER/$name
-                    echo "Backup updated for file $var"
+                    if [[ $? -ne 0 ]] ; then
+                        exit 1
+                    else
+                        echo "Backup updated for file $var"
+                    fi
                 else
+                    name=$name$(date +-%Y-%m-%d-%H-%M-%S)
                     while [ -e $BACKUP_FOLDER/$name ]; do
                         let count++
                         name=$var.$count
@@ -68,25 +72,33 @@ case $1 in
                     mkdir -p ${back_folder%\/*}
                     cp -u -r -i $var $BACKUP_FOLDER/$name
                     echo ${parent_folder%\/*}\|${var##*/}\|$name >> $BACKUP_LIST
-                    echo "Backup created for file $var"
+                    if [[ $? -ne 0 ]] ; then
+                        exit 1
+                    else
+                        echo "Backup created for file $var"
+                    fi
                 fi
             done
 
         else
-            echo "no file specified"
+            echo "No file specified"
         fi
         ;;
     restore)
         if [ $2 ]; then
             if [[ $2 == +([0-9]) ]]
             then
-               line=$(sed -n "$2 p" $BACKUP_LIST)
-               source_folder=${line%%|*};
-               rest=${line#*|}
-               source_file=${rest%%|*};
-               backup_file=${rest#*|}
-               cp -r -i $BACKUP_FOLDER/$backup_file $source_folder/$source_file
-               echo "Data restored to $source_folder/$source_file"
+                line=$(sed -n "$2 p" $BACKUP_LIST)
+                source_folder=${line%%|*};
+                rest=${line#*|}
+                source_file=${rest%%|*};
+                backup_file=${rest#*|}
+                cp -r -i $BACKUP_FOLDER/$backup_file $source_folder/$source_file
+                if [[ $? -ne 0 ]] ; then
+                    exit 1
+                else
+                    echo "Data restored to $source_folder/$source_file"
+                fi
             else
                list_backup
                echo "--------------------------------"
@@ -108,7 +120,11 @@ case $1 in
                rm -rf $BACKUP_FOLDER/$backup_file
                sed "$2 d" $BACKUP_LIST > $BACKUP_FOLDER/.temp
                mv $BACKUP_FOLDER/.temp $BACKUP_LIST
-               echo "Backup file '$source_file' deleted."
+               if [[ $? -ne 0 ]] ; then
+                    exit 1
+               else
+                    echo "Backup file '$source_file' deleted."
+               fi
             else
                list_backup
                echo "--------------------------------"
@@ -123,7 +139,11 @@ case $1 in
         read choice
         if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
             rm -rf $BACKUP_FOLDER
-            echo "All backup data deleted"
+            if [[ $? -ne 0 ]] ; then
+                    exit 1
+            else
+                echo "All backup data deleted"
+            fi
         fi
         ;;
     *)
